b120cec71fcbabc8a365fe5186369b5372c84e93
elenabaurkot <elenabaurkot@gmail.com>
2020-11-02 21:45:55 -0500

    getRandomTrip function started and variables added

--- script.js
+++ script.js
@@ -1,9 +1,30 @@
 // Global Variables
+const randomizerButton = document.getElementById('trip-randomizer');
 // Placeholder variable for now, will obtain this from user input
 let trip = 'Cancun, Mexico';
 
+// Function to show dart throwing animation
+// function showAnimation() {
+
+// }
+
+
+// Function for trip randomizer button click
+function getRandomTrip() {
+  const headerText = document.getElementById('header-text');
+
+  // Get trip location randomly
+
+  // Set the header to tell the user where they're going and change the text of the button
+  headerText.textContent = `You're going to ${trip}!`;
+  randomizerButton.textContent = 'Pick a New Trip';
+}
+
+// On button click you could instead show animation, set a timeout function after animation plays to call get random trip -- animation showing dart first then random trip stuff comes in?
+// Click Events
+randomizerButton.addEventListener('click', getRandomTrip);
+
 // On button click:
-// hide the header
 // animation should pop up showing dart getting thrown at a map--> after the animation then the info about their trip will appear
 //Something should happen that gets a random city or country name, unsure if this should call to some database or api or make an array of possible places
 
